956d40fbddb5804b4bc74eb1a4843d28ceeabe79
Luca <100308395+lucafungo@users.noreply.github.com>
2023-04-13 16:40:14 +0100

    adaptation path

--- script_order.sh
+++ script_order.sh
@@ -1,17 +1,17 @@
-python3 /home/ec2-user/What-the-Guardian-project/api_caller.py
+python3 /home/ec2-user/WTG-project/api_caller.py
 
 sleep 5
 
-python3 /home/ec2-user/What-the-Guardian-project/article_scraper.py
+python3 /home/ec2-user/WTG-project/article_scraper.py
 
 sleep 10
 
-python3 /home/ec2-user/What-the-Guardian-project/blob_analyser.py
+python3 /home/ec2-user/WTG-project/blob_analyser.py
 
 sleep 5
 
-python3 /home/ec2-user/What-the-Guardian-project/email_sender.py
+python3 /home/ec2-user/WTG-project/email_sender.py
 
 sleep 10 
 
-python3 /home/ec2-user/What-the-Guardian-project/directory_cleaner.py
+python3 /home/ec2-user/WTG-project/directory_cleaner.py
